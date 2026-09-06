96b5a2e75d26e1ea3d28ffea4848485a45ade803
Thomas Orlans <thomas.orlans.pro@gmail.com>
2026-09-06 23:02:04 +0200

    add some for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: { port: 8080 },
-  plugins: [
-    tsConfigPaths(),
-    tailwindcss(),
-    tanstackStart({ server: { entry: "server" } }),
-    viteReact(),
-  ],
+  plugins: [tsConfigPaths(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
