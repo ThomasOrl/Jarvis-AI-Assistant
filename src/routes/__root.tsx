babd4b4e5d0693a3ca56f6fede6647965666cd35
Thomas Orlans <thomas.orlans.pro@gmail.com>
2026-09-07 20:19:32 +0200

    change icon onglet

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -85,7 +85,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/logo-jarvis.png", type: "image/png" },
+
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
